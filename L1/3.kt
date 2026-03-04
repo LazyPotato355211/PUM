c085e7360a593277342777b2ff11f0d0fdb54fb2
LazyPotato355211 <355211@uwr.edu.pl>
2026-03-04 19:56:52 +0100

    up

--- L1/3.kt
+++ L1/3.kt
@@ -1,3 +1,24 @@
 fun main(){
-    println(tp(15))
+    trojkat(10)
+}
+fun trojkat (n : Int){
+    val liczba = Array(n) {IntArray(n)}
+
+    for(i in 0 until n){
+        for(s in 0 until (n -i)){
+            print("  ")
+        }
+
+            for(j in 0..i){
+    
+                if(j==0 || j==i){
+                    liczba[i][j] = 1
+                } 
+                else {
+                    liczba[i][j] = liczba[i-1][j-1] + liczba[i-1][j]
+                }
+                System.out.printf("%4d", liczba[i][j])
+            }
+            println()
+    }
 }
