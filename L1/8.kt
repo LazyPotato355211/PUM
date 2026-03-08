cc7443cebb9a881d3907629691a42152d03e162d
LazyPotato355211 <355211@uwr.edu.pl>
2026-03-08 17:51:11 +0100

    8 up

--- L1/8.kt
+++ L1/8.kt
@@ -1,7 +1,17 @@
 fun main(){
-    countVowels("")
+    val wynik = countVowels("Programowanie")
+    println("$wynik")
 }
 
-fun countVowels(word : String){
+fun countVowels(word : String):Int{
+    val samogloski = "aeiouy"
+    var licznik = 0
+    val male = word.lowercase()
 
+    for (i in male){
+        if(i in samogloski){
+            licznik++
+        }
+    }
+    return licznik
 }
