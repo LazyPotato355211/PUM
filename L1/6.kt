24b6c7aee8d892cf621cc6c1370583d8490a50bf
LazyPotato355211 <355211@uwr.edu.pl>
2026-03-08 16:26:28 +0100

    6 up

--- L1/6.kt
+++ L1/6.kt
@@ -1,3 +1,22 @@
 fun main(){
-    println(tp(15))
+    pierwsza(12)
+    pierwsza(7)
+
+}
+
+fun pierwsza(num : Int){
+    if (num <= 1) {
+        println("$num nie jest liczba pierwsza")
+    }
+
+    var sum = 0
+    for(i in 1 until num){
+        if(num%i == 0){
+            sum += i }
+    }
+    if (sum == 1) { 
+        println("$num to liczba pierwsza")}
+    else {
+        println("$num to nie jest liczba pierwsza")}
+        
 }
