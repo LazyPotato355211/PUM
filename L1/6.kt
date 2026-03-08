7af9d8c68a057fd053494256a0340dd9682655da
LazyPotato355211 <355211@uwr.edu.pl>
2026-03-08 16:37:04 +0100

    6 up

--- L1/6.kt
+++ L1/6.kt
@@ -1,14 +1,15 @@
 fun main(){
     println(pierwsza(12))
-
+    println(pierwsza(13))
 }
 
 fun pierwsza(num : Int):Boolean{
     if (num < 2) return false
  
-    for(i in 1 until num){
+    for(i in 2 until num){
         if(num%i == 0){
-            return false }
+            return false
+        }
     }
     return true
         
