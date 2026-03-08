bfc332ffe84ba85e394bba63763593fdc129b3fc
LazyPotato355211 <355211@uwr.edu.pl>
2026-03-08 16:15:53 +0100

    4 up

--- L1/4.kt
+++ L1/4.kt
@@ -1,3 +1,23 @@
+
+// suma alikwotowa sumy s(n) - suma wszystkich dzielnikow wlasciwych liczby n
+
 fun main(){
-    println(tp(15))
+    sprawdz(28)
+    sprawdz(12)
+    sprawdz(8)
+}
+
+fun sprawdz(num : Int){
+    var sum = 0
+    for(i in 1 until num){
+        if(num%i==0){
+            sum += i
+        }
+    }
+
+    when{
+        sum == num -> println("doskonala - $sum")
+        sum > num -> println("obfita - $sum")
+        else -> println("niedomiarowa - $sum")
+    }
 }
